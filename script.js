c5bdece6b2f1369cdb9017bbcf2fd78fa88082b6
Saikasote <166039130+Saikasote@users.noreply.github.com>
2025-05-23 19:04:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".typed-text", {
+  strings: ["Android Developer", "AI Engineer", "ML Engineer", "UI/UX Designer"],
+  typeSpeed: 50,
+  backSpeed: 25,
+  backDelay: 1000,
+  loop: true
+});
